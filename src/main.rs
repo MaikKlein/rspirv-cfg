afbc9e84e7479d86932fb7b04ce5b54442da7c29
khyperia <github@khyperia.com>
2020-11-25 11:10:02 +0100

    Update dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate clap;
 extern crate rspirv_cfg;
-use clap::{App, Arg, SubCommand};
-use rspirv_cfg::{SpirvModule, PetSpirv, export_spirv_cfg };
+use clap::{App, Arg};
+use rspirv_cfg::{export_spirv_cfg, SpirvModule};
 use std::path::PathBuf;
 fn main() {
     let matches = App::new("rspirv-cfg")
@@ -13,7 +13,8 @@ fn main() {
                 .help("Path to the .spv file")
                 .required(true)
                 .takes_value(true),
-        ).get_matches();
+        )
+        .get_matches();
     let file_path = matches.value_of("file").expect("No filename");
     let file_path = PathBuf::from(file_path);
     let module = SpirvModule::load(&file_path);
